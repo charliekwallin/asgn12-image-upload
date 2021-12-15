1b1f4a61e13d316f25e4852148778cf2036b7d69
Charlie Wallin <charliekwallin@gmail.com>
2021-12-15 16:33:00 -0500

    Completed Define Query Methods. Starting Build Objects Using Record Values

--- upload-image.class.php
+++ upload-image.class.php
@@ -3,10 +3,26 @@
 class UploadImage {
 
     // start of active record code
-    
+
     static public $database;
 
+    // TODO: figure out why I can't set this to protected
     static public function set_database($database) {
         self::$database = $database;
     }
+    
+    static public function find_by_sql($sql) {
+        $result = self::$database->query($sql);
+        if(!$result) {
+            exit("Database query failed.");
+        }
+        return $result;
+    }
+
+    static public function find_all() {
+        $sql = "SELECT * FROM images ORDER BY id DESC";
+        return self::find_by_sql($sql);
+    }
+
+
 }
--- view.php
+++ view.php
@@ -5,13 +5,12 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>View Birds</title>
 </head>
 <body>
     <a href="index.php">&#8592;</a>
     <?php
-    $sql = "SELECT * FROM images ORDER BY id DESC";
-    $result = UploadImage::$database->query($sql);
+    $result = UploadImage::find_all();
     $row = $result->fetch_assoc();
     $result->free();
     
